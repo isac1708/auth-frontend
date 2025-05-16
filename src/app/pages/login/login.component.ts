95826a4ac3bb554a787d0e4759c7f94f6a370845
isac1708 <isac.figueredo@gmail.com>
2025-05-16 12:12:26 -0300

    feat: implementa tela de login e integração com AuthService

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html'
 })
 export class LoginComponent {
+  username = '';
+  password = '';
 
+  constructor(private authService: AuthService) {}
+
+  onSubmit() {
+    this.authService.login(this.username, this.password).subscribe({
+      next: (res) => {
+        this.authService.saveToken(res.token);
+        alert('Login realizado com sucesso!');
+      },
+      error: () => {
+        alert('Usuário ou senha inválidos.');
+      }
+    });
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+interface LoginResponse {
+  token: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = 'http://localhost:5023/api/user';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.apiUrl}/login`, { username, password });
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('authToken', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('authToken');
+  }
+
+  logout() {
+    localStorage.removeItem('authToken');
+  }
 }
